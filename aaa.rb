11544822e46d2210ccf3a2165aa0cd1da9a99543
i09158knct <i09158knct@gmail.com>
2012-05-20 11:18:18 +0900

    refactoring

--- aaa.rb
+++ aaa.rb
@@ -8,12 +8,8 @@ aa = KnowledgeBase.new
 csv = CSV.open('all_data.csv', 'r')
 
 csv.each do |row|
-  if row[2].split(' ')[1]
-    row[2].split(' ').each do |emotion|
+  row[2].split(' ').each do |emotion|
       aa.add_knowledge [row[1], emotion]
-    end
-  else
-    aa.add_knowledge [row[1], row[2]]
   end
 end
 
