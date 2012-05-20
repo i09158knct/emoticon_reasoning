7211fe3481233833b27828e156fdb02da7b70ad6
i09158knct <i09158knct@gmail.com>
2012-05-21 00:50:15 +0900

    Slightly modified.

--- backward_reasoning.rb
+++ backward_reasoning.rb
@@ -3,16 +3,16 @@ require './data_getter.rb'
 
 CSV_FILE_PATH = './all_data.csv'
 
-emotion_k = get_emotion_knowledges(CSV_FILE_PATH)
-emoticon_k = get_emoticon_knowledges(CSV_FILE_PATH)
+emotion_knowledges = get_emotion_knowledges(CSV_FILE_PATH)
+emoticon_knowledges = get_emoticon_knowledges(CSV_FILE_PATH)
 
 
 emoticon = ARGV[0] || 'orz'
 
-emotions = emoticon_k.get_conditions_from emoticon
+emotions = emoticon_knowledges.get_conditions_from emoticon
 sentences = []
 emotions.each do |emotion|
-   sentences += emotion_k.get_conditions_from(emotion)
+   sentences += emotion_knowledges.get_conditions_from emotion
 end
 sentences.uniq!
 
